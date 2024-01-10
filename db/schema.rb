7207f74680e79a79e0ad1bd61204d0b1d6fe8213
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2024-01-10 10:13:38 -0400

    Add foreign keys and indexes to all tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_10_134509) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_140906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_134509) do
     t.string "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_10_134509) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "searches", "users"
 end

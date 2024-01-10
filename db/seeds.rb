2c843352be316b1950f5c691b8121971775fee92
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2024-01-10 11:16:32 -0400

    Add seed data for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+articles = Article.create([
+  { title: 'Effective Project Management Strategies', content: 'In today\'s dynamic business environment, effective project management is crucial for success. This article explores proven strategies and best practices for achieving project goals.' },
+  { title: 'Navigating Leadership Challenges in a Globalized World', content: 'Leadership in a globalized context comes with unique challenges. This article discusses key challenges faced by leaders and offers insights into navigating complexities and fostering team success.'},
+  { title: 'Strategies for Enhancing Workplace Productivity', content: 'Productivity is a key driver of organizational success. Learn practical strategies and techniques to enhance workplace productivity and create a positive work environment.'},
+])
+
+puts 'Seeds loaded successfully!'

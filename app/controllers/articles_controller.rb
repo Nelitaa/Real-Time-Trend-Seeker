9f8701141d09fd3446368669e05c8a20cb4318ec
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2024-01-11 10:34:10 -0400

    reformulate article search with pg_search gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,16 +7,10 @@ class ArticlesController < ApplicationController
     if term.present?
       current_user.save
       current_user.searches.create(term: term)
+      @articles = Article.search_full_text(term)
+    else
+      @articles = Article.all
     end
-
-    @articles = if term.present?
-                  Article.select('*')
-                  .select(Arel.sql("CASE WHEN title ILIKE #{ActiveRecord::Base.connection.quote("%#{term}%")} THEN 0 ELSE 1 END AS title_priority"))
-                  .where('title ILIKE ? OR content ILIKE ?', "%#{term}%", "%#{term}%")
-                  .order('title_priority, title')
-                else
-                  Article.all
-                end
 
     @popular_searches = Search.group(:term).order('count_term DESC').limit(3).count(:term)
 
